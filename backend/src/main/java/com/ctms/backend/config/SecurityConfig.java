89f0e24135ca550c7310a0359b3f22ab320d5b2b
saum7033 <rajsaumya7033@gmail.com>
2026-03-24 13:25:22 +0530

    refactors needed

--- backend/src/main/java/com/ctms/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ctms/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ctms.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.*;
 

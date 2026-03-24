2e3a441f09da8be0efbd90cf92545cb5b28ad1b3
saum7033 <rajsaumya7033@gmail.com>
2026-03-24 14:12:56 +0530

    fix of cors

---
+++ backend/src/main/java/com/ctms/backend/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.ctms.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("CTMS Backend is running!");
+    }
+}
